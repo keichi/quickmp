1468975e56f536c909bd335150ac1cbdd697cfcd
Keichi Takahashi <hello@keichi.dev>
2026-01-16 11:32:56 +0900

    Add compute_squared_sum function for sliding window squared sum calculation

--- src/ve/internal.hpp
+++ src/ve/internal.hpp
@@ -10,6 +10,7 @@ void sliding_window_dot_product_fft(const double *T, const double *Q, double *QT
 void sliding_window_dot_product_naive(const double *T, const double *Q, double *QT, size_t n,
                                       size_t m);
 void compute_mean_std(const double *T, double *mu, double *sigma, size_t n, size_t m);
+void compute_squared_sum(const double *T, double *sum, size_t n, size_t m);
 void selfjoin(const double *T, double *P, size_t n, size_t m);
 void abjoin(const double *T1, const double *T2, double *P, size_t n1, size_t n2, size_t m);
 
